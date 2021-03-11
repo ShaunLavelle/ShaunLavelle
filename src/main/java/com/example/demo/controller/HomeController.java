8f37efb7dc34a63b44137d3a30856751a779ac54
Shaun Lavelle <shaunlavelle@hotmail.co.uk>
2021-03-11 21:49:36 +0000

    Added server port

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.example.demo.entity.User;
 import com.example.demo.service.UserService;
 
 @Controller
@@ -14,10 +13,10 @@ public class HomeController {
 	@Autowired
 	UserService userSerivce;
 
-	@GetMapping(path = {"/", "/home"})
+	@GetMapping(path = { "/", "/home" })
 	public String index(Model model) {
 
-		userSerivce.save(new User("Shaun", 23));
+//		userSerivce.save(new User("Shaun", 23));
 
 //		userSerivce.findByName("Shaun");
 		model.addAttribute("msg", "Welcome");
