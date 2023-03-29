ef564b9f2f9ef5394d5ba1782bf2aba60e5960a8
slavelle <slavelle@jaguarlandrover.com>
2023-03-29 14:02:25 +0100

    Added package distribution

--- src/main/java/com/example/shaunlavelle/ShaunLavelleApplication.java
+++ src/main/java/com/example/shaunlavelle/ShaunLavelleApplication.java
@@ -1,4 +1,4 @@
-package com.example.sl;
+package com.example.shaunlavelle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/example/shaunlavelle/controller/HomeController.java
+++ src/main/java/com/example/shaunlavelle/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.example.sl.controller;
+package com.example.shaunlavelle.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
